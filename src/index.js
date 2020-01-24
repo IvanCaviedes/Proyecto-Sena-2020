5c5f317d5971e08c5e389d28654e882c40bd0955
Ivan Caviedes <ivancaviedes99@gmail.com>
2020-01-24 17:07:58 -0500

    comentario2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express =  require('express');
 const app = express();
-// hola
+// Coneccion con base de datos
 require('./database');
 //confugurando el puerto
 app.set('port',process.env.PORT || 3000);
