4b886b179a178a71692f54ba71cbf69ac340a3f3
Ivan Caviedes <ivancaviedes99@gmail.com>
2020-01-24 18:11:02 -0500

    incluyendo react con webpack

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class app extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hola</h1>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express =  require('express');
 const app = express();
+const path = require('path');
 // Coneccion con base de datos
 require('./database');
 //confugurando el puerto
 app.set('port',process.env.PORT || 3000);
+//Archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));;
 //iniciando servidor
 app.listen(app.get('port'),()=>{
     console.log('El servidor esta corriendo en: ', app.get('port'));
