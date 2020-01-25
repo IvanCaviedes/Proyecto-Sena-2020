dabb4d5800e8863cf67f16bb126c0ce4f54fda34
andresballen9 <andresballen9@gmail.com>
2020-01-24 21:33:05 -0500

    eslider de bureba con unicornios

--- src/app/app.js
+++ src/app/app.js
@@ -4,8 +4,30 @@ export default class app extends Component {
     render() {
         return (
             <div>
-                <h1>hola</h1>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="img/logotipos-de-unicornios-7.png" alt="First slide" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="img/logotipos-de-unicornios-28.jpg" alt="Second slide" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="img/logotipos-de-unicornios-7.png" alt="Third slide" />
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
             </div>
+
         )
     }
 }
