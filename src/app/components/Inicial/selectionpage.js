da0e9b1e0387aa70b6fc882573996ba2d68cb900
andresballen9 <andresballen9@gmail.com>
2020-01-26 13:19:24 -0500

    basura

--- src/app/components/Inicial/selectionpage.js
+++ src/app/components/Inicial/selectionpage.js
@@ -5,29 +5,29 @@ export default class selectionpage extends Component {
         return (
             <div class="page-section home-page">
                 <div class="hero-slider owl-carousel">
-                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="img/slider-bg-1.jpg" data-hash="slide-1">
-                        <div class="si-text-box">
+                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="https://i.imgur.com/KBzejqS.jpg" data-hash="slide-1">
+                        <div  class="si-text-box transpaert">
                             <span>Mascotas </span>
                             <h2>Cuidados</h2>
                             <p>Para Perros y Gatos</p>
                             <a href="" class="site-btn">LEER MÁS</a>
                         </div>
-                        <div class="next-slide-show set-bg" data-setbg="img/slider-bg-2.jpg">
+                        <div class="next-slide-show set-bg" data-setbg="https://i.imgur.com/VRaEppj.jpg">
                             <a href="#slide-2" class="ns-btn">Next</a>
                         </div>
                     </div>
-                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="img/slider-bg-2.jpg" data-hash="slide-2">
+                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="https://i.imgur.com/VRaEppj.jpg" data-hash="slide-2">
                         <div class="si-text-box">
                             <span>Mascotas</span>
                             <h2>Razas</h2>
                             <p>Para Perros y Gatos</p>
                             <a href="" class="site-btn">LEER MÁS</a>
                         </div>
-                        <div class="next-slide-show set-bg" data-setbg="img/slider-bg-1.jpg">
+                        <div class="next-slide-show set-bg" data-setbg="https://i.imgur.com/1ZfnfOO.jpg">
                             <a href="#slide-3" class="ns-btn">Next</a>
                         </div>
                     </div>
-                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="img/slider-bg-2.jpg" data-hash="slide-2">
+                    <div class="slider-item d-flex align-items-center set-bg" data-setbg="https://i.imgur.com/1ZfnfOO.jpg" data-hash="slide-2">
                         <div class="si-text-box">
                             <span>mascotas</span>
                             <h2>Alimentación</h2>
