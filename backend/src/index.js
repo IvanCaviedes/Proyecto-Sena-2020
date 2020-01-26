bbb78a9f57e3a02a91dd65e857a21864d54ed7e2
Ivan Caviedes <ivancaviedes99@gmail.com>
2020-01-26 13:06:48 -0500

    renovando el año :v

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express =  require('express');
+const app = express();
+// Coneccion con base de datos
+require('./database');
+//confugurando el puerto
+app.set('port',process.env.PORT || 4000);
+//iniciando servidor
+app.listen(app.get('port'),()=>{
+    console.log('El servidor esta corriendo en: ', app.get('port'));
+});
