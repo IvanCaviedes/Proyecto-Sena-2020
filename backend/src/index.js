f793e71999ac04d63391f84821d1511a62600b10
Ivan Caviedes <ivancaviedes99@gmail.com>
2020-01-26 19:45:18 -0500

    añadiendo documentacion a backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ const app = express();
 require('./database');
 //confugurando el puerto
 app.set('port',process.env.PORT || 4000);
+
+app.use(__dirname,'public')
+
 //iniciando servidor
 app.listen(app.get('port'),()=>{
     console.log('El servidor esta corriendo en: ', app.get('port'));
