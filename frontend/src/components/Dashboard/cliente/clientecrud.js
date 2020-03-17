19f9d25f7661f046133ac49fbadfef8782881e11
andresballen9 <andresballen9@gmail.com>
2020-03-17 15:11:33 -0500

    k

--- frontend/src/components/Dashboard/cliente/clientecrud.js
+++ frontend/src/components/Dashboard/cliente/clientecrud.js
@@ -266,7 +266,7 @@ export default class users extends Component {
     render() {
         return (
             <div>
-                <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style={{ minHeight: '600px', backgroundImage: 'url(http://localhost:4000/public/img/notocar/producto.png)', backgroundSize: 'cover', backgroundPosition: 'center top' }}>
+                <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style={{ minHeight: '600px', backgroundImage: 'url(https://www.ittrends.es/files/201707/tipos-clientes-banca.jpg)', backgroundSize: 'cover', backgroundPosition: 'center top' }}>
                     <span class="mask bg-gradient-default opacity-8"></span>
                     <div class="container-fluid d-flex align-items-center">
                         <div class="row">
